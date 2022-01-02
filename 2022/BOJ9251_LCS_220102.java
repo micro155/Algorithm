05846b1a404fd3d72a22b4c1a7b40739cccb65d2
micro155 <qawe2006@naver.com>
2022-01-02 23:06:52 +0900

    22/01/02 문제풀이 업로드

---
+++ 2022/BOJ9251_LCS_220102.java
@@ -0,0 +1,35 @@
+package Algorithm_2022.algorithm_0102;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ9251_LCS_220102 {
+
+    private static char str1[];
+    private static char str2[];
+    private static int dp[][];
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        str1 = br.readLine().toCharArray();
+        str2 = br.readLine().toCharArray();
+
+        dp = new int[str1.length + 1][str2.length + 1];
+
+        for (int i = 1; i <= str1.length; i++) {
+            for (int j = 1; j <= str2.length; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[str1.length][str2.length]);
+
+    }
+
+}
