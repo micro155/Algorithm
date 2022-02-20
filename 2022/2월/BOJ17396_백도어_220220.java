e91476b9bee50a2a174c80078fb036c3cefbd95a
micro155 <qawe2006@naver.com>
2022-02-20 15:56:31 +0900

    BOJ17396 백로드 문제풀이

--- 2022/2월/BOJ17396_백도어_220220.java
+++ 2022/2월/BOJ17396_백도어_220220.java
@@ -7,24 +7,28 @@ import java.util.*;
 public class BOJ17396_백도어_220220 {
 
     private static int N, M;
-    private static int d[];
+    private static long d[];
     private static Set<Integer> set;
     private static PriorityQueue<Pair> pq;
+    private static ArrayList<Pair> list[];
+    private static boolean visited[];
 
     private static class Pair implements Comparable<Pair> {
-        int start;
         int end;
-        int distance;
+        long distance;
 
-        public Pair(int start, int end, int distance) {
-            this.start = start;
+        public Pair(int end, long distance) {
             this.end = end;
             this.distance = distance;
         }
 
         @Override
         public int compareTo(Pair o) {
-            return this.start - o.start;
+            if (this.distance - o.distance > 0) {
+                return 1;
+            } else {
+                return -1;
+            }
         }
     }
 
@@ -37,17 +41,23 @@ public class BOJ17396_백도어_220220 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        set = new HashSet<>();
         pq = new PriorityQueue<>();
-        d = new int[N];
+        d = new long[N];
+        visited = new boolean[N];
+        set = new HashSet<>();
+        list = new ArrayList[N];
 
-        Arrays.fill(d, Integer.MAX_VALUE);
+        Arrays.fill(d, Long.MAX_VALUE);
+
+        for (int i = 0; i < N; i++) {
+            list[i] = new ArrayList<>();
+        }
 
         st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(st.nextToken());
-            if (num == 1 && i < N - 1) {
+            if (i > 0 && i < N && num == 1) {
                 set.add(i);
             }
         }
@@ -57,28 +67,36 @@ public class BOJ17396_백도어_220220 {
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int distance = Integer.parseInt(st.nextToken());
-            pq.add(new Pair(start, end, distance));
-            pq.add(new Pair(end, start, distance));
+            list[start].add(new Pair(end, distance));
+            list[end].add(new Pair(start, distance));
         }
 
         d[0] = 0;
-        while(!pq.isEmpty()) {
+
+        pq.offer(new Pair(0, 0));
+        while (!pq.isEmpty()) {
             Pair p = pq.poll();
 
-            if (set.contains(p.end) || set.contains(p.start)) {
+            if (visited[p.end]) {
                 continue;
             }
-            if (d[p.end] > d[p.start] + p.distance) {
-                d[p.end] = d[p.start] + p.distance;
-            }
-            if (p.start > p.end) {
-                if (d[p.start] > d[p.end] + p.distance) {
-                    d[p.start] = d[p.end] + p.distance;
+
+            visited[p.end] = true;
+
+            for (int i = 0; i < list[p.end].size(); i++) {
+                Pair next = list[p.end].get(i);
+
+                if (next.end != N - 1 && set.contains(next.end)) {
+                    continue;
+                }
+                if (d[next.end] > d[p.end] + next.distance) {
+                    d[next.end] = d[p.end] + next.distance;
+                    pq.offer(new Pair(next.end, d[next.end]));
                 }
             }
         }
 
-        if (d[N - 1] == Integer.MAX_VALUE) {
+        if (d[N - 1] == Long.MAX_VALUE) {
             System.out.println(-1);
         } else {
             System.out.println(d[N - 1]);
