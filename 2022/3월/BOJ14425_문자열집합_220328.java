103c385dbc878cbcd1c13df4faffce12410af29b
micro155 <qawe2006@naver.com>
2022-03-28 22:45:21 +0900

    BOJ14425 문자열 집합 문제풀이 업로드

---
+++ 2022/3월/BOJ14425_문자열집합_220328.java
@@ -0,0 +1,38 @@
+package algorithm_220328;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BOJ14425_문자열집합_220328 {
+
+
+    private static int N, M;
+    private static Set<String> set;
+    private static int result;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        set = new HashSet<>();
+        result = 0;
+
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            if (set.contains(br.readLine())) {
+                result++;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
