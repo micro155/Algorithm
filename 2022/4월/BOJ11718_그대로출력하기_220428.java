842140a8f068e9828c9df0dea2d7f58bf02364f1
micro155 <qawe2006@naver.com>
2022-04-28 22:16:48 +0900

    BOJ11718 그대로 출력하기 문제풀이 업로드

---
+++ 2022/4월/BOJ11718_그대로출력하기_220428.java
@@ -0,0 +1,15 @@
+package algorithm_220428;
+
+import java.util.Scanner;
+
+public class BOJ11718_그대로출력하기_220428 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
