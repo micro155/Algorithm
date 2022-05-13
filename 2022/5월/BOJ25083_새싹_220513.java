ac9395bc19cacf1041a6e5181555c5803aba72b6
micro155 <qawe2006@naver.com>
2022-05-13 23:37:16 +0900

    BOJ25083 새싹 문제풀이 업로드

---
+++ 2022/5월/BOJ25083_새싹_220513.java
@@ -0,0 +1,14 @@
+package algorithm_220513;
+
+public class BOJ25083_새싹_220513 {
+
+    public static void main(String[] args) {
+
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
