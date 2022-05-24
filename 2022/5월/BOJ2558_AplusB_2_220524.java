1594d8ed0d2c2ecbfd558bcd518b57f533fd0e0a
micro155 <qawe2006@naver.com>
2022-05-24 21:44:52 +0900

    BOJ 2558 A+B - 2 문제풀이 업로드

---
+++ 2022/5월/BOJ2558_AplusB_2_220524.java
@@ -0,0 +1,17 @@
+package algorithm_220524;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ2558_AplusB_2_220524 {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        System.out.println(A + B);
+    }
+}
