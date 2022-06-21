e5608e2c7ff6cc1eba3e378f65b72ed83e0462c9
micro155 <qawe2006@naver.com>
2022-06-21 21:58:41 +0900

    BOJ17298 오큰수 문제풀이 업로드

---
+++ 2022/6월/BOJ17298_오큰수_220621.java
@@ -0,0 +1,45 @@
+package algorithm_220621;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ17298_오큰수_220621 {
+
+    private static int N;
+    private static int arr[];
+    private static Stack<Integer> stack;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        stack = new Stack<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            arr[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
